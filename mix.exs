906ca0c43081963703be89c156144952e37071b3
AlefMach <alef_mac@hotmail.com>
2023-08-14 23:34:36 -0300

    add docs deps

--- mix.exs
+++ mix.exs
@@ -29,7 +29,9 @@ defmodule RememberMe.MixProject do
 
   # Run "mix help deps" to learn about dependencies.
   defp deps do
-    []
+    [
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false}
+    ]
   end
 
   defp package do
